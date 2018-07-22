829b13eecc91dcd24f4421cb33604d1f615fb93b
Lalo Espino <lalospn@gmail.com>
2018-07-22 17:36:49 -0500

    Initial Commit

---
+++ Chat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Lalo Espino on 22/07/18.
+//  Copyright © 2018 Lalo Espino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
