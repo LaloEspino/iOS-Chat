c39ac26302c1d54b12d795170478de39d84885a9
Lalo Espino <lalospn@gmail.com>
2018-07-22 20:05:20 -0500

    Input container, login button, name text field added. Video 29:47

--- Chat/Controller/LoginController.swift
+++ Chat/Controller/LoginController.swift
@@ -9,25 +9,72 @@
 import UIKit
 
 class LoginController: UIViewController {
+    
+    // MARK: Components of the ViewController
+    
+    let inputsContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 5
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    let loginRegisterButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = UIColor(r: 171, g: 42, b: 62)
+        button.setTitle("Register", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 5
+        button.layer.masksToBounds = true
+        button.setTitleColor(UIColor.white, for: .normal)
+        return button
+    }()
+    
+    let nameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Name"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    // MARK: viewDidLoad
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        view.backgroundColor = UIColor(r: 176, g: 54, b: 72)
         view.backgroundColor = UIColor(r: 128, g: 0, b: 0)
 
-        let inputsContainerView = UIView()
-        inputsContainerView.backgroundColor = UIColor.white
-        inputsContainerView.translatesAutoresizingMaskIntoConstraints = false
-        
         view.addSubview(inputsContainerView)
+        view.addSubview(loginRegisterButton)
         
-        // MARK: Constainst go here
-        
+        setupInputsContainerView()
+        setupLoginRegisterButton()
+    }
+    
+    // MARK: Constainst go here
+
+    func setupInputsContainerView() {
         inputsContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         inputsContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         inputsContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
         inputsContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        
+        inputsContainerView.addSubview(nameTextField)
+        
+        nameTextField.leftAnchor.constraint(equalTo: inputsContainerView.leftAnchor, constant: 12).isActive = true
+        nameTextField.topAnchor.constraint(equalTo: inputsContainerView.topAnchor).isActive = true
+        nameTextField.widthAnchor.constraint(equalTo: inputsContainerView.widthAnchor).isActive = true
+        nameTextField.heightAnchor.constraint(equalTo: inputsContainerView.heightAnchor, multiplier: 1 / 3).isActive = true
+    }
+    
+    func setupLoginRegisterButton() {
+        loginRegisterButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        loginRegisterButton.topAnchor.constraint(equalTo: inputsContainerView.bottomAnchor, constant: 12).isActive = true
+        loginRegisterButton.widthAnchor.constraint(equalTo: inputsContainerView.widthAnchor).isActive = true
+        loginRegisterButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        loginRegisterButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
     }
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
